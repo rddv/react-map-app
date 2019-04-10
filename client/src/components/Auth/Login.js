f08944f31fcd9dd0af4756f7eb3d26f822dac03d
denys.r <denys.r@sbtech.com>
2019-04-10 17:21:18 +0300

    added login, token for front-end side

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { GoogleLogin } from 'react-google-login';
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+
+  function onSuccess(googleUser) {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log('google user ----> ', idToken )
+  }
+
+  return <GoogleLogin 
+      clientId="421757943354-2tve5sr13b7utfgo0g88k6s0k2s7v09c.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />;
 };
 
 const styles = {
